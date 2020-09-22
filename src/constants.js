763eceb4b68032d6c984cb764e5d0c6627334d19
Carlos Lancha <carlos.lancha@liferay.com>
2020-09-22 18:52:05 +0200

    Get data from DXP

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const AUTH = 'Basic dGVzdEBsaWZlcmF5LmNvbTp0ZXN0';
+
+const DOMAIN = 'http://localhost:8080';
+
+const GRAPHQL_ENDPOINT = `${DOMAIN}/o/graphql`;
+
+export {
+    AUTH,
+    DOMAIN,
+    GRAPHQL_ENDPOINT
+};
