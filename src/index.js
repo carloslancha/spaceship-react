937534327fd2f03c16c9b6ad82e7a2ab761beea7
Carlos Lancha <carlos.lancha@liferay.com>
2020-09-23 13:20:24 +0200

    Create project with liferay-js generator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
-import { AUTH, GRAPHQL_ENDPOINT } from './constants';
 
-const apolloClient = new ApolloClient({
-	headers: {
-		'Authorization': AUTH
-	},
-	uri: GRAPHQL_ENDPOINT
-});
+/**
+ * This is the main entry point of the portlet.
+ *
+ * See https://tinyurl.com/js-ext-portlet-entry-point for the most recent 
+ * information on the signature of this function.
+ *
+ * @param  {Object} params a hash with values of interest to the portlet
+ * @return {void}
+ */
+export default function main({portletNamespace, contextPath, portletElementId, configuration}) {
 
-ReactDOM.render(
-    <ApolloProvider client={apolloClient}>
-        <App />
-    </ApolloProvider>,
-    document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+}
